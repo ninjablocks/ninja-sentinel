8acebf89f03275c8061a16660e5d170c849b4950
Jeremy Manoto <jeremy@manoto.com.au>
2012-12-21 17:03:51 +1100

    Add Delete Zone functionality

--- yeoman/app/scripts/services/ZoneService.js
+++ yeoman/app/scripts/services/ZoneService.js
@@ -5,22 +5,26 @@ yeomanApp.factory('ZoneService'
   , function($rootScope, $http, NinjaUtilities) {
   var zoneService = {
 
+
+    Zones: null,
+
     /**
      * Get all the zones
      */
     GetZones: function(callback) {
 
-      $http.get("/zone").success(function(response) {
-        console.log("GetZones:", response);
+      $http.get('/zone').success(function(response) {
+        console.log('GetZones:', response);
+        this.Zones = NinjaUtilities.ObjectArrayToArray(response);
         if (callback) {
           callback(response);
         }
-      }).error(function(response) {
-        console.log("GetZones (FAILED):", response);
+      }.bind(this)).error(function(response) {
+        console.log('GetZones (FAILED):', response);
         if (callback) {
           callback(response);
         }
-      });
+      }.bind(this));
     },
 
 
@@ -30,8 +34,8 @@ yeomanApp.factory('ZoneService'
      * @param {Function} callback Optional callback function
      */
     GetZone: function(zoneId, callback) {
-      $http.get("/zone/" + zoneId).success(function(response) {
-        console.log("GetZone:", response);
+      $http.get('/zone/' + zoneId).success(function(response) {
+        console.log('GetZone:', response);
         if (callback) {
           callback(response);
         }
@@ -43,21 +47,34 @@ yeomanApp.factory('ZoneService'
      * @param {Zone} zone Zone to save
      */
     SaveZone: function(zone, callback) {
-      if (zone.Options.id) {
+      if (zone.id) {
         // Exists. Update only
-        $http.put("/zone/" + zone.Options.id, zone.Options, { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json'} }).success(function(response) {
-          console.log("SaveZone Existing:", response);
+        $http.put('/zone/' + zone.id, zone.Options, { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json'} }).success(function(response) {
+          console.log('SaveZone Existing:', response);
           if (callback) {
             callback(response);
           }
         });
       } else {
         // Create new
-        $http.post("/zone", zone.Options, { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json'} }).success(function(response) {
-          console.log("SaveZone New:", response);
+        $http.post('/zone', zone.Options, { headers: { 'Content-Type': 'application/json', 'Accept': 'application/json'} }).success(function(response) {
+          console.log('SaveZone New:', response);
           if (callback) {
             callback(response);
           }
+        });
+      }
+    },
+
+    /**
+     * Delete a zone
+     * @param {Zone}   zone     Zone object to delete
+     * @param {Function} callback Callback function
+     */
+    DeleteZone: function(zone, callback) {
+      if (zone.id) {
+        $http.delete('/zone/' + zone.id).success(function(response) {
+          console.log("DeleteZone:", response, zone);
         });
       }
     }
