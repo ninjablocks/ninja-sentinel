63ea484e9f4566e2ff22871592b3e3b5c649665a
Jeremy Manoto <jeremy@manoto.com.au>
2012-12-31 16:52:17 +1100

    Hooked UI Override to Override controller

--- yeoman/app/scripts/controllers/utilitybar.js
+++ yeoman/app/scripts/controllers/utilitybar.js
@@ -1,24 +1,29 @@
 'use strict';
 
 yeomanApp.controller('UtilitybarCtrl'
-  , ['$scope', '$http'
-  , function($scope, $http) {
+  , ['$scope', '$http', 'UIEvents'
+  , function($scope, $http, UIEvents) {
   
+
+    $scope.Override = false;
 
     /**
      * Sets the system override
      * @param {bool|null} value true|false|null
-     */ 
-    $scope.Override = function(value) {
+     */
+    $scope.SetOverride = function(value) {
 
       var data = {
-        override: value.toString()
+        override: (value === null) ? "null" : value.toString()
       };
 
+
+
       $http.put('/override', data).success(function(response) {
-        console.log("PUT /override", response);
+        // console.log("PUT /override", response);
+        $scope.Override = value;
       }).error(function(response) {
-        console.log("PUT /override", response);
+        // console.log("PUT /override", response);
       });
     };
 
@@ -28,11 +33,21 @@ yeomanApp.controller('UtilitybarCtrl'
     $scope.GetOverride = function() {
 
       $http.get('/override').success(function(response) {
-        console.log("GET /override", response);
+        // console.log("GET /override", response);
+
+        $scope.$broadcast(UIEvents.OverrideUpdate, response.override);
+        $scope.Override = response.override;
+
       }).error(function(response) {
-        console.log("GET /override", response);
+        // console.log("GET /override", response);
+
       });
     };
 
     $scope.GetOverride();
+
+
+    $scope.$on(UIEvents.OverrideUpdate, function(event, override) {
+      $scope.Override = override;
+    });
 }]);
